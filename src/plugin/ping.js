c885767e82cc5727d3ed323e0feee75d32837347
HANSAMAL/LK <atayafuataya@gmail.com>
2024-07-16 10:09:46 +0530

    𝙃𝘼𝙉𝙎𝘼𝙈𝘼𝙇-𝛭𝐷

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -4,11 +4,11 @@ const ping = async (m, sock) => {
 
   if (cmd === "ping") {
     const start = new Date().getTime();
-    await m.React('⚡');
+    await m.React('🔥');
     const end = new Date().getTime();
     const responseTime = (end - start) / 1000;
 
-    const text = `*_🔥⃝вσт ѕρєє∂: ${responseTime.toFixed(2)} s_*`;
+    const text = `*_⃝𝙃𝘼𝙉𝙎𝘼𝙈𝘼𝙇 вσт ѕρєє∂: ${responseTime.toFixed(2)} s_*`;
     sock.sendMessage(m.from, { text }, { quoted: m });
   }
 }
