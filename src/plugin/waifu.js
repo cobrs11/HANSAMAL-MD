9dbf51c880580f8c52e0ab73df5528ac8e839cd4
HANSAMAL/LK <atayafuataya@gmail.com>
2024-07-20 14:16:25 +0530

    Update waifu.js

--- src/plugin/waifu.js
+++ src/plugin/waifu.js
@@ -9,7 +9,7 @@ const stickerCommandHandler = async (m, gss) => {
   const stickerCommands = ['cry', 'kiss', 'kill', 'kick', 'hug', 'pat', 'lick', 'bite', 'yeet', 'bully', 'bonk', 'wink', 'poke', 'nom', 'slap', 'smile', 'wave', 'awoo', 'blush', 'smug', 'dance', 'happy', 'sad', 'cringe', 'cuddle', 'shinobu', 'handhold', 'glomp', 'highfive'];
 
   if (stickerCommands.includes(cmd)) {
-    const packname = `Ethix-MD`;
+    const packname = `HANSAMAL-MD`;
     const author = '';
 
     try {
