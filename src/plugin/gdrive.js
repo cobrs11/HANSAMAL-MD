44d1366d36f7db3b8203f7a6a57f20ac3d3226fc
HANSAMAL/LK <atayafuataya@gmail.com>
2024-07-20 14:12:17 +0530

    Update gdrive.js

--- src/plugin/gdrive.js
+++ src/plugin/gdrive.js
@@ -20,7 +20,7 @@ const gdriveDownload = async (m, Matrix) => {
 
       if (gdriveInfo && gdriveInfo.status && gdriveInfo.data) {
         const mediaUrl = gdriveInfo.data;
-        const caption = `> © Powered By Ethix-MD`;
+        const caption = `> © Powered By 🌐IMALKA-HANSAMAL`;
 
         // Inferring the file type based on the file extension
         const extension = mediaUrl.split('.').pop().toLowerCase();
