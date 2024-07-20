313a837ec335bdb62edf3cb164c3519f8434760a
HANSAMAL/LK <atayafuataya@gmail.com>
2024-07-20 13:13:35 +0530

    Update ping.js

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -8,7 +8,7 @@ const ping = async (m, sock) => {
     const end = new Date().getTime();
     const responseTime = (end - start) / 1000;
 
-    const text = `*_🔥⃝вσт ѕρєє∂: ${responseTime.toFixed(2)} s_*`;
+    const text = `*_🔥HANSAMAL-MD SPEED: ${responseTime.toFixed(2)} s_*`;
     sock.sendMessage(m.from, { text }, { quoted: m });
   }
 }
