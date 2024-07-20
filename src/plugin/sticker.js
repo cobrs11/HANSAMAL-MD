94c5c5a1368b05704afbd820526487006e6e4eaf
HANSAMAL/LK <atayafuataya@gmail.com>
2024-07-20 14:09:15 +0530

    Update sticker.js

--- src/plugin/sticker.js
+++ src/plugin/sticker.js
@@ -7,7 +7,7 @@ const stickerCommand = async (m, gss) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
 
   const packname = "";
-  const author = "𝐄𝐭𝐡𝐢𝐱-𝐌𝐃";
+  const author = "IMALKA-HANSAMAL";
 
   const validCommands = ['sticker', 's'];
 
