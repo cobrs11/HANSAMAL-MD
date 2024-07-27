e3cb5827abfc3efc43a960cb79cf643d02943cab
KGRashmikasarojm <isururashmika34@gmail.com>
2024-07-28 04:53:56 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "YOUR_ID",
+  SESSION_ID: process.env.SESSION_ID || "MD&1d6Lgv8T",
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
@@ -19,7 +19,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "✪𝙃𝘼𝙉𝙎𝘼𝙈𝘼𝙇",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "94711262551",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "9478368488",
   GEMINI_KEY: process.env.GEMINI_KEY || "",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
