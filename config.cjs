d943b29e220a1f9c3af1470e370fdde3d824c72b
HANSAMAL/LK <atayafuataya@gmail.com>
2024-08-02 16:44:04 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -18,7 +18,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "✪𝙃𝘼𝙉𝙎𝘼𝙈𝘼𝙇",
+  OWNER_NAME: process.env.OWNER_NAME || "▶𝙷𝙰𝙽𝚂𝙰𝙼𝙰𝙻༒",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "94711262551",
   GEMINI_KEY: process.env.GEMINI_KEY || "",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
