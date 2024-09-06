db33ddf49513a43a04c55dd48642b8c180f423ee
HANSAMAL OFFICIAL <atayafuataya@gmail.com>
2024-09-06 13:56:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function downloadSessionData() {
         console.error('Please add your session to SESSION_ID env !!');
         return false;
     }
-    const sessdata = config.SESSION_ID.split("HANSAMAL-MD=")[1];
+    const sessdata = config.SESSION_ID.split("HANSAMAL-MD=", "")[1];
     const url = `https://mega.nz/file/${sessdata}`;
     try {
         const response = await axios.get(url);
